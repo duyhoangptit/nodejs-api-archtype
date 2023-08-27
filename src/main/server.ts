283e44e82e18417afd8e8693720983c4d8c1ce31
Ta Duy Hoang <duyhoangptit@gmail.com>
2023-08-27 22:04:03 +0700

    config env with dotenv

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,14 @@
 import 'module-alias/register'
 import setupApp from '@main/config/app'
 import env from '@main/config/env'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const nodeEnv = process.env.APP_ENV
+
+dotenv.config({
+  path: `./environments/.env.${nodeEnv}`
+})
 
 const app = setupApp()
 
