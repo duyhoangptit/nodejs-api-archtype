72a502b41cf3d73619478c5bf74a13234d48b2c0
Ta Duy Hoang <duyhoangptit@gmail.com>
2023-09-19 22:25:49 +0700

    update archtype using typescript

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,20 @@
+import * as dotEnv from 'dotenv'
+
+const nodeEnv = process.env.NODE_ENV ?? 'dev'
+
+if (nodeEnv !== 'prod') {
+  const configFile = `./environments/.env.${nodeEnv}`
+  dotEnv.config({ path: configFile })
+} else {
+  dotEnv.config()
+}
+
 export default {
-  port: process.env.PORT ?? 3000,
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongodb:27017/catalog'
+  PORT: process.env.PORT ?? 3000,
+  DB_URL: process.env.MONGODB_URI ?? 'mongodb://mongodb:27017/catalog',
+  APP_SECRET: process.env.APP_SECRET,
+  EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+  MSG_QUEUE_URL: process.env.MSG_QUEUE_URL,
+  CUSTOMER_SERVICE: 'customer_service',
+  SHOPPING_SERVICE: 'shopping_service',
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,15 +1,7 @@
 import 'module-alias/register'
 import setupApp from '@main/config/app'
 import env from '@main/config/env'
-import * as dotenv from 'dotenv'
-dotenv.config()
-
-const nodeEnv = process.env.APP_ENV
-
-dotenv.config({
-  path: `./environments/.env.${nodeEnv}`
-})
 
 const app = setupApp()
 
-app.listen(env.port, () => { console.log(`Server running on port ${env.port}`) })
+app.listen(env.PORT, () => { console.log(`Server running on port ${env.PORT}`) })
